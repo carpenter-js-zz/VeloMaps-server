1c9984a08a75dad9efcd00abb810a7ff94c240d9
carpenter-js <carpenterbarrett@gmail.com>
2018-12-31 09:50:04 -0700

    set up skeleton, single get returning hardcode route

--- index.js
+++ index.js
@@ -22,6 +22,17 @@ app.use(
   })
 );
 
+app.get('/api/routes', (req, res, next) => {
+  res.json([
+    {lat: 39.753761766463, lng: -104.99863849925396},
+    {lat: 39.750396366621935, lng: -104.99413238810894},
+    {lat: 39.746601846089845, lng: -104.9990247373521},
+    {lat: 39.745967232716154, lng: -104.99799138551589},
+    {lat: 39.74309643014503, lng: -105.00193959718581}
+  ]);
+  next();
+});
+
 function runServer(port = PORT) {
   const server = app
     .listen(port, () => {
